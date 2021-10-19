72db9c5b125396c8e6815e8dd40a72cd1c06a2a2
stephb9959 <stephane.bourque@gmail.com>
2021-10-18 23:10:25 -0700

    Initial Commit

---
+++ src/Dashboard.h
@@ -0,0 +1,26 @@
+//
+//	License type: BSD 3-Clause License
+//	License copy: https://github.com/Telecominfraproject/wlan-cloud-ucentralgw/blob/master/LICENSE
+//
+//	Created by Stephane Bourque on 2021-03-04.
+//	Arilia Wireless Inc.
+//
+#ifndef UCENTRALGW_DASHBOARD_H
+#define UCENTRALGW_DASHBOARD_H
+
+#include "OpenWifiTypes.h"
+#include "RESTAPI_ProvObjects.h"
+
+namespace OpenWifi {
+	class SubDashboard {
+	  public:
+			void Create();
+			[[nodiscard]] const ProvObjects::Report & Report() const { return DB_;}
+			inline void Reset() { LastRun_=0; DB_.reset(); }
+	  private:
+	        ProvObjects::Report  	DB_{};
+			uint64_t 				LastRun_=0;
+	};
+}
+
+#endif // UCENTRALGW_DASHBOARD_H
