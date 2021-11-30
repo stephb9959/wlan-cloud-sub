7be872974313622b194ea0e5640fb7ffedb1c18f
stephb9959 <stephane.bourque@gmail.com>
2021-11-30 15:05:18 -0800

    Adding pass through authentication.

--- src/RESTAPI/RESTAPI_oauth2_handler.cpp
+++ src/RESTAPI/RESTAPI_oauth2_handler.cpp
@@ -10,19 +10,19 @@ namespace OpenWifi {
 
     void RESTAPI_oauth2_handler::DoPost() {
         std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-        API_Proxy(Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/suboauth2");
+        API_Proxy(Logger_, Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/suboauth2");
         std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
     }
 
     void RESTAPI_oauth2_handler::DoGet() {
         std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-        API_Proxy(Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/suboauth2");
+        API_Proxy(Logger_, Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/suboauth2");
         std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
     }
 
     void RESTAPI_oauth2_handler::DoDelete() {
         std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-        API_Proxy(Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/suboauth2");
+        API_Proxy(Logger_, Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/suboauth2");
         std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
     }
 
--- src/framework/API_Proxy.h
+++ src/framework/API_Proxy.h
@@ -8,71 +8,75 @@
 #include "Poco/JSON/Parser.h"
 
 namespace OpenWifi {
-    void API_Proxy(Poco::Net::HTTPServerRequest *Request,
-                   Poco::Net::HTTPServerResponse *Response,
-                   const char * ServiceType,
-                   const char * PathRewrite,
-                   uint64_t msTimeout_ = 10000 ) {
-        std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-
-        auto Services = MicroService::instance().GetServices(ServiceType);
-        std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-        for(auto const &Svc:Services) {
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-            Poco::URI   SourceURI(Request->getURI());
-            Poco::URI	DestinationURI(Svc.PrivateEndPoint);
+    void API_Proxy( Poco::Logger &Logger,
+                    Poco::Net::HTTPServerRequest *Request,
+                    Poco::Net::HTTPServerResponse *Response,
+                    const char * ServiceType,
+                    const char * PathRewrite,
+                    uint64_t msTimeout_ = 10000 ) {
+        try {
             std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
 
-            DestinationURI.setPath(PathRewrite);
-            DestinationURI.setQuery(SourceURI.getQuery());
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << DestinationURI.getHost() << ":" << DestinationURI.getPort() << std::endl;
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << DestinationURI.getPathAndQuery() << std::endl;
-
-            Poco::Net::HTTPSClientSession Session(DestinationURI.getHost(), DestinationURI.getPort());
+            auto Services = MicroService::instance().GetServices(ServiceType);
             std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+            for(auto const &Svc:Services) {
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+                Poco::URI   SourceURI(Request->getURI());
+                Poco::URI	DestinationURI(Svc.PrivateEndPoint);
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
 
-            Session.setTimeout(Poco::Timespan(msTimeout_/1000, msTimeout_ % 1000));
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+                DestinationURI.setPath(PathRewrite);
+                DestinationURI.setQuery(SourceURI.getQuery());
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << DestinationURI.getHost() << ":" << DestinationURI.getPort() << std::endl;
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << DestinationURI.getPathAndQuery() << std::endl;
 
-            Poco::Net::HTTPRequest ProxyRequest(Request->getMethod(),
-                                                DestinationURI.getPathAndQuery(),
-                                                Poco::Net::HTTPMessage::HTTP_1_1);
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-            ProxyRequest.add("X-API-KEY", Svc.AccessKey);
-            ProxyRequest.add("X-INTERNAL-NAME", MicroService::instance().PublicEndPoint());
-            ProxyRequest.setContentType("application/json");
+                Poco::Net::HTTPSClientSession Session(DestinationURI.getHost(), DestinationURI.getPort());
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
 
-            Poco::JSON::Parser P;
-            auto Body = P.parse(Request->stream()).extract<Poco::JSON::Object::Ptr>();
-            std::stringstream SS;
-            Poco::JSON::Stringifier::stringify(Body,SS);
+                Session.setTimeout(Poco::Timespan(msTimeout_/1000, msTimeout_ % 1000));
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
 
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << " >>> " << SS.str() << std::endl;
+                Poco::Net::HTTPRequest ProxyRequest(Request->getMethod(),
+                                                    DestinationURI.getPathAndQuery(),
+                                                    Poco::Net::HTTPMessage::HTTP_1_1);
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+                ProxyRequest.add("X-API-KEY", Svc.AccessKey);
+                ProxyRequest.add("X-INTERNAL-NAME", MicroService::instance().PublicEndPoint());
+                ProxyRequest.setContentType("application/json");
 
-            std::ostream & os = Session.sendRequest(ProxyRequest);
+                Poco::JSON::Parser P;
+                auto Body = P.parse(Request->stream()).extract<Poco::JSON::Object::Ptr>();
+                std::stringstream SS;
+                Poco::JSON::Stringifier::stringify(Body,SS);
 
-            os << SS.str() ;
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << " >>> " << SS.str() << std::endl;
 
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+                std::ostream & os = Session.sendRequest(ProxyRequest);
+                os << SS.str() ;
 
-            for(const auto &i:ProxyRequest) {
-                std::cout << "   " << i.first << ":" << i.second << std::endl;
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+
+                for(const auto &i:ProxyRequest) {
+                    std::cout << "   " << i.first << ":" << i.second << std::endl;
+                }
+
+                Poco::Net::HTTPResponse ProxyResponse;
+                std::istream &is = Session.receiveResponse(ProxyResponse);
+                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+
+                if(ProxyResponse.getStatus()==Poco::Net::HTTPResponse::HTTP_OK) {
+                    std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+                    std::stringstream   SS;
+                    ProxyResponse.write(SS);
+                    std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+                    Response->read(SS);
+                    Response->setStatus(Poco::Net::HTTPResponse::HTTP_OK);
+                    std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
+                    return;
+                }
             }
-
-            Poco::Net::HTTPResponse ProxyResponse;
-            std::istream &is = Session.receiveResponse(ProxyResponse);
-            std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-
-            if(ProxyResponse.getStatus()==Poco::Net::HTTPResponse::HTTP_OK) {
-                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-                std::stringstream   SS;
-                ProxyResponse.write(SS);
-                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-                Response->read(SS);
-                Response->setStatus(Poco::Net::HTTPResponse::HTTP_OK);
-                std::cout << __FILE__ << " : " << __func__  << " : " << __LINE__ << std::endl;
-                return;
-            }
+        } catch (const Poco::Exception &E) {
+            Logger.log(E);
         }
     }
 }
