09859998fb3b37949278602132be3b186b4ead94
stephb9959 <stephane.bourque@gmail.com>
2021-12-01 20:38:22 -0800

    Adding MFA to API.

--- src/RESTAPI/RESTAPI_mfa_handler.cpp
+++ src/RESTAPI/RESTAPI_mfa_handler.cpp
@@ -3,15 +3,16 @@
 //
 
 #include "RESTAPI_mfa_handler.h"
+#include "framework/API_Proxy.h"
 
 namespace OpenWifi {
 
-    void RESTAPI_mfa_handler::DoGet() {
-
+    void RESTAPI_mfa_handler::DoPut() {
+        API_Proxy(Logger_, Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/submfa");
     }
 
-    void RESTAPI_mfa_handler::DoPut() {
-
+    void RESTAPI_mfa_handler::DoGet() {
+        API_Proxy(Logger_, Request, Response, OpenWifi::uSERVICE_SECURITY.c_str(),"/api/v1/submfa");
     }
 
 }
