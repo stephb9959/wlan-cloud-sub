550e05cd7c770ce3a4975ba580fb75b0ca1f347f
stephb9959 <stephane.bourque@gmail.com>
2022-02-24 14:38:40 -0800

    Adding default config creation.

--- src/RESTAPI/RESTAPI_signup_handler.cpp
+++ src/RESTAPI/RESTAPI_signup_handler.cpp
@@ -8,18 +8,22 @@
 namespace OpenWifi {
 
     void RESTAPI_signup_handler::DoPost() {
+        std::cout << "Doing signup DoPost" << std::endl;
         return API_Proxy(Logger(), Request, Response, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
     void RESTAPI_signup_handler::DoPut() {
+        std::cout << "Doing signup DoPut" << std::endl;
         return API_Proxy(Logger(), Request, Response, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
     void RESTAPI_signup_handler::DoGet() {
+        std::cout << "Doing signup DoGet" << std::endl;
         return API_Proxy(Logger(), Request, Response, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
     void RESTAPI_signup_handler::DoDelete() {
+        std::cout << "Doing signup DoDelete" << std::endl;
         return API_Proxy(Logger(), Request, Response, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
